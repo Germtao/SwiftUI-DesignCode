e9a173ba0d40babc77de6c167c9e6fd060338609
QDSG <piftao1127@163.com>
2019-12-16 11:24:27 +0800

    Initial Commit

---
+++ SwiftUI-DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-DesignCode
+//
+//  Created by QDSG on 2019/12/16.
+//  Copyright © 2019 unitTao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
