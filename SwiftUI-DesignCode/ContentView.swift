f76501a1af9de564c02d68022b97096b16dddadb
QDSG <piftao1127@163.com>
2019-12-16 14:49:03 +0800

    Layout(布局) & Stack(堆栈)

--- SwiftUI-DesignCode/ContentView.swift
+++ SwiftUI-DesignCode/ContentView.swift
@@ -10,20 +10,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        ZStack {
             VStack {
-                Text("UI Design")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color("accent"))
-                    .padding(.top)
-                Text("Certificate")
-                    .foregroundColor(Color.white)
+                Text("Card Back")
             }
-            Image("Background")
+            .frame(width: 270.0, height: 220.0)
+            .background(Color.blue)
+            .cornerRadius(10.0)
+            .shadow(radius: 10)
+            .offset(x: 0.0, y: -40)
+            
+            VStack {
+                Text("Card Back")
+            }
+            .frame(width: 310.0, height: 220.0)
+            .background(Color.pink)
+            .cornerRadius(10.0)
+            .shadow(radius: 10)
+            .offset(x: 0.0, y: -20)
+
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("UI Design")
+                            .font(.headline)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color("accent"))
+                            .padding(.top)
+                        Text("Certificate")
+                            .foregroundColor(Color.white)
+                            .padding(.top, 8.0)
+                    }
+                    Spacer()
+                    Image("Logo")
+                        .resizable()
+                        .frame(width: 30.0, height: 30.0)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Image("Background")
+            }
+            .frame(width: 340.0, height: 220.0)
+            .background(Color.black)
+            .cornerRadius(10.0)
+            .shadow(radius: 20)
         }
-        .background(Color.black)
-        .cornerRadius(10.0)
     }
 }
 
