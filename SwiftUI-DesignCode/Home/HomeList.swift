fe1a9cf67106942cf4040a786fea8512365cd4ed
QDSG <piftao1127@163.com>
2019-12-17 16:36:58 +0800

    模态跳转

--- SwiftUI-DesignCode/Home/HomeList.swift
+++ SwiftUI-DesignCode/Home/HomeList.swift
@@ -9,13 +9,22 @@
 import SwiftUI
 
 struct HomeList: View {
+    @State var showCourse = false
+    
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
-            HStack {
-                ForEach(0 ..< 3) { item in
-                    CourseView()
+            HStack(spacing: 20.0) {
+                ForEach(courseData) { course in
+                    Button(action: { self.showCourse.toggle() }) {
+                        CourseView(course: course)
+                    }.sheet(isPresented: self.$showCourse, onDismiss: {
+                        self.showCourse = false
+                    }) {
+                        ContentView()
+                    }
                 }
             }
+            .padding(.horizontal, 20)
             .frame(height: 480)
         }
     }
@@ -28,21 +37,50 @@ struct HomeList_Previews: PreviewProvider {
 }
 
 struct CourseView: View {
+    var course: Course
     var body: some View {
         VStack(alignment: .leading) {
-            Text("Build an app with SwiftUI")
+            Text(course.title)
                 .font(.title)
                 .fontWeight(.bold)
                 .foregroundColor(.white)
                 .padding(20)
                 .lineLimit(4)
-                .frame(width: 150)
+                .padding(.trailing, 50)
             Spacer()
-            Image("Illustration1")
+            Image(course.image)
+                .resizable()
+                .renderingMode(.original)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 246, height: 150)
+                .padding(.bottom, 30)
         }
-        .background(Color("background3"))
+        .background(course.color)
         .cornerRadius(30)
         .frame(width: 246, height: 360)
-        .shadow(color: Color("backgroundShadow3"), radius: 20, x: 0, y: 20)
+        .shadow(color: course.shadowColor, radius: 20, x: 0, y: 20)
     }
 }
+
+struct Course: Identifiable {
+    var id = UUID()
+    var title: String
+    var image: String
+    var color: Color
+    var shadowColor: Color
+}
+
+let courseData = [
+    Course(
+        title: "Build an app with SwiftUI",
+        image: "Illustration1",
+        color: Color("background3"),
+        shadowColor: Color("backgroundShadow3")
+    ),
+    Course(
+        title: "Flutter For Designers",
+        image: "Illustration2",
+        color: Color("background4"),
+        shadowColor: Color("backgroundShadow4")
+    ),
+]
